a150b4d22538dbfa737e252c65b064503015e9e8
태규 <123963462+EOTAEGYU@users.noreply.github.com>
2024-08-05 21:26:14 +0900

    feat: Naver, Google OAuth 사용하여 회원정보 MySQL에 저장

---
+++ OAuthJWT/src/main/java/com/example/oauthjwt/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.example.oauthjwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+    @GetMapping("/my")
+    @ResponseBody
+    public String myPage() {
+        return "my route";
+    }
+}
